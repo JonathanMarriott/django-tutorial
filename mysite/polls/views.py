9d0cde1f8dff0c88799c52964db650f0ca95ce0c
Jonathan Marriott <marriottjonny@gmail.com>
2022-06-24 17:18:33 +0100

    Add some views to questions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
 
 from django.http import HttpResponse
 
+from .models import Question
+### VERBOSE
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {'latest_question_list': latest_question_list,}
+#     return HttpResponse(template.render(context,request))
+
 def index(request):
-    return HttpResponse("Hello world. You're at polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list,}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
